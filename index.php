8e9045dcb68dfede16860d6150d23a3e3232d978
Lorenzo-Arnau <skyline03@hotmail.it>
2021-03-09 17:32:50 +0100

    first connection a create a new tab and insert first users

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "root";
-$dbname = "db_hotel";
+$dbname = "test_db";
 
 // Connect
 $conn = new mysqli($servername, $username, $password, $dbname);
@@ -15,35 +15,30 @@ if ($conn && $conn->connect_error) {
   echo "Connection done. <br/>";
 }
 
-$sql = "SELECT ospiti.name, ospiti.lastname, prenotazioni_has_ospiti.prenotazione_id FROM ospiti JOIN prenotazioni_has_ospiti ON ospiti.id = prenotazioni_has_ospiti.ospite_id";
+$sql = "CREATE TABLE ospiti (
+    id INT(6)  AUTO_INCREMENT PRIMARY KEY,
+    firstname VARCHAR(30) NOT NULL,
+    lastname VARCHAR(30) NOT NULL,
+    email VARCHAR(50),
+    reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+    ) ";
+$sql = "INSERT INTO ospiti (firstname, lastname, email)
+VALUES ('Mario', 'Bianchi', 'Bianchi@example.com')";
 $result = $conn->query($sql);
 
-if ($result && $result->num_rows > 0) {
-    // output data of each row
-    echo  "QUERY : Stampare tutti gli ospiti per ogni prenotazione";
-    echo  "</br></br>----------------------------------------------------------------------------";
-    while($row = $result->fetch_assoc()) {
-        echo  "</br>";
-        echo "<div>". "nome ospite  ". $row['name']. " cognome: ".$row['lastname'] . " prenotazione id: ".$row['prenotazione_id'] . "</div>";
-        echo  "----------------------------------------------------------------------------------";
-  }
-} elseif ($result) {
-    echo "0 results";
-} else {
-    echo "query error";
-}
+// if ($result && $result->num_rows > 0) {
+//     // output data of each row
+//     echo  "QUERY : Stampare tutti gli ospiti per ogni prenotazione";
+//     echo  "</br></br>----------------------------------------------------------------------------";
+//     while($row = $result->fetch_assoc()) {
+//         echo  "</br>";
+//         echo "<div>". "nome ospite  ". $row['name']. " cognome: ".$row['lastname'] . " prenotazione id: ".$row['prenotazione_id'] . "</div>";
+//         echo  "----------------------------------------------------------------------------------";
+//   }
+// } elseif ($result) {
+//     echo "0 results";
+// } else {
+//     echo "query error";
+// }
 $conn->close();
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-
-    
-</body>
-</html>
